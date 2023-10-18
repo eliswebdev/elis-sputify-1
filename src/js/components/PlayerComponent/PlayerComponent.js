edcca5202ebb0234f0900131cc714532a89c3014
Daniel Di Pietro <daniel.dipietro@eurobet.it>
2023-10-18 12:19:46 +0200

    prot

--- src/js/components/PlayerComponent/PlayerComponent.js
+++ src/js/components/PlayerComponent/PlayerComponent.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
 const PlayerComponent = () => {
-        return(
-            <section className="player layout__player">
-            <button id="play-icon">
-                Play
-              </button>
-              <div id="audio-player-container">
-                <span id="current-time" className="time">
-                  0:00
-                </span>
-                <input type="range" id="seek-slider" max={100} defaultValue={0} />
-                <span id="duration" className="time">
-                  0:00
-                </span>
-              </div>
-            </section>
-            )
+  return (
+    <section className="player layout__player">
+      <button id="play-icon">
+        Play
+      </button>
+      <div id="audio-player-container">
+        <span id="current-time" className="time">
+          0:00
+        </span>
+        <input type="range" id="seek-slider" max={100} defaultValue={0} />
+        <span id="duration" className="time">
+          0:00
+        </span>
+      </div>
+    </section>
+  )
 }
 
 export default PlayerComponent;
