5a6ad914ff15fc3b9ebe1897df8944dc1a7c68b2
carlo.ferrari <carlo.ferrari@eurobet.it>
2023-10-18 10:50:05 +0200

    ciao Daniel 2

--- src/js/components/RootComponent/RootComponent.js
+++ src/js/components/RootComponent/RootComponent.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import "../../../scss/clientlib.main.scss";
+import PlayerComponent from '../PlayerComponent/PlayerComponent';
+import PlayListComponent from '../PlayListComponent/PlayListComponent'
+import TrackListComponent from '../TrackListComponent/TrackListComponent';
 
 class RootComponent extends React.Component {
   constructor(props) {
@@ -8,217 +11,17 @@ class RootComponent extends React.Component {
 
   render() {
     return (<>
-    <main class="layout">
-      <div class="layout__main-wrapper">
-        <aside class="sidebar layout__sidebar">
-          <div class="heading">Sputofy</div>
+    <main className="layout">
+      <div className="layout__main-wrapper">
+        <aside className="sidebar layout__sidebar">
+          <div className="heading">Sputofy</div>
         </aside>
-        <div class="layout__content">
-          <section class="playlist layout__playlist">
-            <div class="heading">Playlists</div>
-            <div class="playlist__cards">
-              <section class="playlist__card">
-                <div class="playlist__card-coverarea">
-                  <img class="playlist__card-img" src= "https://unsplash.it/600/400" />
-                </div>
-                <div class="playlist__card-title">playlist title</div>
-              </section>
-              <section class="playlist__card">
-                <div class="playlist__card-coverarea">
-                  <img class="playlist__card-img" src= "https://unsplash.it/600/400" />
-                </div>
-                <div class="playlist__card-title">playlist title</div>
-              </section>
-              <section class="playlist__card">
-                <div class="playlist__card-coverarea">
-                  <img class="playlist__card-img" src= "https://unsplash.it/600/400" />
-                </div>
-                <div class="playlist__card-title">playlist title</div>
-              </section>
-              <section class="playlist__card">
-                <div class="playlist__card-coverarea">
-                  <img class="playlist__card-img" src= "https://unsplash.it/600/400" />
-                </div>
-                <div class="playlist__card-title">playlist title</div>
-              </section>
-              <section class="playlist__card">
-                <div class="playlist__card-coverarea">
-                  <img class="playlist__card-img" src= "https://unsplash.it/600/400" />
-                </div>
-                <div class="playlist__card-title">playlist title</div>
-              </section>
-              <section class="playlist__card">
-                <div class="playlist__card-coverarea">
-                  <img class="playlist__card-img" src= "https://unsplash.it/600/400" />
-                </div>
-                <div class="playlist__card-title">playlist title</div>
-              </section>
-              <section class="playlist__card">
-                <div class="playlist__card-coverarea">
-                  <img class="playlist__card-img" src= "https://unsplash.it/600/400" />
-                </div>
-                <div class="playlist__card-title">playlist title</div>
-              </section>
-              <section class="playlist__card">
-                <div class="playlist__card-coverarea">
-                  <img class="playlist__card-img" src= "https://unsplash.it/600/400" />
-                </div>
-                <div class="playlist__card-title">playlist title</div>
-              </section>
-              <section class="playlist__card">
-                <div class="playlist__card-coverarea">
-                  <img class="playlist__card-img" src= "https://unsplash.it/600/400" />
-                </div>
-                <div class="playlist__card-title">playlist title</div>
-              </section>
-              <section class="playlist__card">
-                <div class="playlist__card-coverarea">
-                  <img class="playlist__card-img" src= "https://unsplash.it/600/400" />
-                </div>
-                <div class="playlist__card-title">playlist title</div>
-              </section>
-            </div>
-            <div class="heading">Tracklist</div>
-          </section>
-          <section class="trackslist layout__trackslist">
-            <table class="trackslist__table">
-              <tr>
-                <th>Title</th>
-                <th>Album</th>
-                <th>Duration</th>
-              </tr>
-              <tr>
-                <td>Alfreds Futterkiste</td>
-                <td>Maria Anders</td>
-                <td>4:55</td>
-              </tr>
-              <tr>
-                <td>Dolor sit amet</td>
-                <td>Francisco Chang</td>
-                <td>3:24</td>
-              </tr>
-              <tr>
-              <td>Alfreds Futterkiste</td>
-              <td>Maria Anders</td>
-              <td>4:55</td>
-              </tr>
-            <tr>
-              <td>Dolor sit amet</td>
-              <td>Francisco Chang</td>
-              <td>3:24</td>
-            </tr>
-            <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>4:55</td>
-            </tr>
-          <tr>
-            <td>Dolor sit amet</td>
-            <td>Francisco Chang</td>
-            <td>3:24</td>
-          </tr>
-          <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>4:55</td>
-          </tr>
-        <tr>
-          <td>Dolor sit amet</td>
-          <td>Francisco Chang</td>
-          <td>3:24</td>
-        </tr>
-        <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Maria Anders</td>
-        <td>4:55</td>
-        </tr>
-      <tr>
-        <td>Dolor sit amet</td>
-        <td>Francisco Chang</td>
-        <td>3:24</td>
-      </tr>
-      <tr>
-        <td>Dolor sit amet</td>
-        <td>Francisco Chang</td>
-        <td>3:24</td>
-      </tr>
-      <tr>
-      <td>Alfreds Futterkiste</td>
-      <td>Maria Anders</td>
-      <td>4:55</td>
-      </tr>
-    <tr>
-      <td>Dolor sit amet</td>
-      <td>Francisco Chang</td>
-      <td>3:24</td>
-    </tr>
-    <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>4:55</td>
-    </tr>
-    <tr>
-      <td>Dolor sit amet</td>
-      <td>Francisco Chang</td>
-      <td>3:24</td>
-    </tr>
-    <tr>
-      <td>Dolor sit amet</td>
-      <td>Francisco Chang</td>
-      <td>3:24</td>
-    </tr>
-    <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>4:55</td>
-    </tr>
-    <tr>
-      <td>Dolor sit amet</td>
-      <td>Francisco Chang</td>
-      <td>3:24</td>
-    </tr>
-    <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>4:55</td>
-    </tr>
-    <tr>
-      <td>Dolor sit amet</td>
-      <td>Francisco Chang</td>
-      <td>3:24</td>
-    </tr>
-    <tr>
-      <td>Dolor sit amet</td>
-      <td>Francisco Chang</td>
-      <td>3:24</td>
-    </tr>
-    <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>4:55</td>
-    </tr>
-    <tr>
-      <td>Dolor sit amet</td>
-      <td>Francisco Chang</td>
-      <td>3:24</td>
-    </tr>
-    <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>4:55</td>
-    </tr>
-    <tr>
-      <td>Dolor sit amet</td>
-      <td>Francisco Chang</td>
-      <td>3:24</td>
-    </tr>
-    </table>
-          </section>
+        <div className="layout__content">
+          <PlayListComponent />
+          <TrackListComponent />
         </div>
       </div>
-      <section class="player layout__player">
-        player
-      </section>
+      <PlayerComponent />
     </main></>);
   }
 }
