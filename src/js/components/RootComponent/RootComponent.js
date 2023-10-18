bfa138377071f1251183857c345f4925036b5bff
Daniel Di Pietro <daniel.dipietro@eurobet.it>
2023-10-18 10:53:09 +0200

    added player

--- src/js/components/RootComponent/RootComponent.js
+++ src/js/components/RootComponent/RootComponent.js
@@ -217,7 +217,19 @@ class RootComponent extends React.Component {
         </div>
       </div>
       <section class="player layout__player">
-        player
+        <button id="play-icon">
+          Play
+        </button>
+        <div id="audio-player-container">
+
+          <span id="current-time" className="time">
+            0:00
+          </span>
+          <input type="range" id="seek-slider" max={100} defaultValue={0} />
+          <span id="duration" className="time">
+            0:00
+          </span>
+        </div>
       </section>
     </main></>);
   }
